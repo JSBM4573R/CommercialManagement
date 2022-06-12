c981ae7b125552b64887ce0d5c08a1031cac1067
Juan Sebastian <juan_seb07@hotmail.com>
2022-06-12 18:43:44 -0500

    Proyecto base Commercial Management + configuracion en application.properties y carpetas.

---
+++ src/main/java/com/jsbm/commercialmanagement/CommercialManagementApplication.java
@@ -0,0 +1,13 @@
+package com.jsbm.commercialmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercialManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommercialManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsbm/commercialmanagement/CommercialManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsbm.commercialmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommercialManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
